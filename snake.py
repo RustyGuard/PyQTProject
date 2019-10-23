8b71436cd8b64182115b1f7e95fd3b7e7b940911
RustyGuard <44666045+RustyGuard@users.noreply.github.com>
2019-10-23 17:21:18 +0400

    Locker for snake.
    Some improvements.

--- snake.py
+++ snake.py
@@ -1,6 +1,6 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtWidgets import QMessageBox, QInputDialog
 
 from ui_snake import Ui_MainWindow
 from game import Game
@@ -9,8 +9,9 @@ import random
 
 class SnakeWindow(Game, Ui_MainWindow):
     def __init__(self, parent):
-        super().__init__(parent, 350)
+        super().__init__(parent, 250)
         print('Snake!')
+        self.locked = False
         self.GRID_SIZE = 20
         self.dir = 0
         self.direction = [[0, -1], [1, 0], [0, 1], [-1, 0]]
@@ -21,23 +22,23 @@ class SnakeWindow(Game, Ui_MainWindow):
         self.colors = {
             'nothing': QColor(125, 125, 125),
             'fruit': QColor(255, 100, 100),
-            'body': QColor(100, 255, 100),
-            'head': QColor(50, 255, 50)
+            'body': QColor(100, 200, 100),
+            'head': QColor(0, 200, 0)
         }
         for i, btn in enumerate([self.btn0, self.btn1, self.btn2, self.btn3]):
             btn.direction = i
             btn.clicked.connect(self.twist)
 
     def gen_fruit(self):
-        x, y = random.randint(0, 9), random.randint(0, 9)
+        x, y = random.randint(0, self.GRID_SIZE - 1), random.randint(0, self.GRID_SIZE - 1)
         while self.grid[x][y] != 0:
-            x, y = random.randint(0, 9), random.randint(0, 9)
+            x, y = random.randint(0, self.GRID_SIZE - 1), random.randint(0, self.GRID_SIZE - 1)
         self.grid[x][y] = -1
 
-    def get_tile(self, x, y):
+    def get_tile(self, x, y, default=-2):
         if 0 <= x < self.GRID_SIZE and 0 <= y < self.GRID_SIZE:
             return self.grid[x][y]
-        return -2
+        return default
 
     # Return last tile value
     def move_snake(self):
@@ -45,21 +46,37 @@ class SnakeWindow(Game, Ui_MainWindow):
         self.snake_info[0] += dir[0]
         self.snake_info[1] += dir[1]
         tile = self.get_tile(self.snake_info[0], self.snake_info[1])
-        self.grid[self.snake_info[0]][self.snake_info[1]] = self.snake_info[2] + 1
+        if tile != -2:
+            self.grid[self.snake_info[0]][self.snake_info[1]] = self.snake_info[2] + 1
         return tile
 
     def twist(self):
         self.dir = self.sender().direction
+        self.updateLabel()
+
+    def updateLabel(self):
+        arrows = ['↑', '→', '↓', '←']
+        self.label.setText(arrows[self.dir])
 
     def keyPressEvent(self, e):
-        if e.key() == Qt.Key_W:
-            self.dir = 0
-        elif e.key() == Qt.Key_D:
-            self.dir = 1
-        elif e.key() == Qt.Key_S:
-            self.dir = 2
-        elif e.key() == Qt.Key_A:
-            self.dir = 3
+        keys = [Qt.Key_W, Qt.Key_D, Qt.Key_S, Qt.Key_A]
+        try:
+            index = keys.index(e.key())
+        except ValueError:
+            index = -1
+        if index != -1:
+            if self.locked and (self.dir + index) % 2 == 0:
+                print('Sorry it is locked')
+                return
+            self.dir = index
+            self.updateLabel()
+        if e.key() == Qt.Key_L:
+            self.locked = not self.locked
+            print(self.locked)
+            if self.locked:
+                self.label_2.setText('🔐')
+            else:
+                self.label_2.setText('🔓')
 
     def OnUpdate(self, delta):
         tile = self.move_snake()
@@ -86,15 +103,15 @@ class SnakeWindow(Game, Ui_MainWindow):
         for x in range(self.GRID_SIZE):
             for y in range(self.GRID_SIZE):
                 if x == self.snake_info[0] and y == self.snake_info[1]:
-                    qp.fillRect(x * cell_size, y * cell_size, cell_size, cell_size, self.colors['head'])
+                    qp.fillRect(x * (cell_size + 1), y * (cell_size + 1), cell_size, cell_size, self.colors['head'])
                     continue
                 tile = self.grid[x][y]
                 if tile == 0:
-                    qp.fillRect(x * cell_size, y * cell_size, cell_size, cell_size, self.colors['nothing'])
+                    qp.fillRect(x * (cell_size + 1), y * (cell_size + 1), cell_size, cell_size, self.colors['nothing'])
                 elif tile == -1:
-                    qp.fillRect(x * cell_size, y * cell_size, cell_size, cell_size, self.colors['fruit'])
+                    qp.fillRect(x * (cell_size + 1), y * (cell_size + 1), cell_size, cell_size, self.colors['fruit'])
                 else:
-                    qp.fillRect(x * cell_size, y * cell_size, cell_size, cell_size, self.colors['body'])
+                    qp.fillRect(x * (cell_size + 1), y * (cell_size + 1), cell_size, cell_size, self.colors['body'])
 
     def getName(self):
         return 'Snake'
