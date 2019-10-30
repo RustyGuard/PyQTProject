00a958498073d9a0f253662829e4e0cd263d039d
RustyGuard <44666045+RustyGuard@users.noreply.github.com>
2019-10-30 18:13:51 +0400

    Release 1.0.0
    Game is playable. Some ui changes.

--- Board.py
+++ Board.py
@@ -1,20 +1,22 @@
 import sqlite3
-# import pymorphy2
+import pymorphy2
 import random
 
+
 class Board:
-    def __init__(self):
+    def __init__(self, log):
         self.let_con = sqlite3.connect('res/letters.db')
-        # self.morph = pymorphy2.MorphAnalyzer()
-        with open('res/word_rus.txt', mode='r', encoding='utf-8') as words:
-            self.words = map(lambda x: x.replace('\n', ''), words.readlines())
+        self.morph = pymorphy2.MorphAnalyzer()
+        self.words = []
+        self.log = log
+        # with open('res/word_rus.txt', mode='r', encoding='utf-8') as words:
+        # self.words = map(lambda x: x.replace('\n', ''), words.readlines())
 
     def generate(self):
         self.grid = [[''] * 16 for _ in range(16)]
         self.chips = []
         cur = self.let_con.cursor()
         for i in cur.execute('''SELECT * FROM letters''').fetchall():
-            # print(i)
             for j in range(i[2]):
                 self.chips.append(i[1])
         self.chips = random.sample(self.chips, len(self.chips))
@@ -31,16 +33,86 @@ class Board:
     def update_chips(self, btns):
         for i, btn in zip(self.curr_chips, btns):
             btn.setText(i)
+            if i != '':
+                btn.setToolTip(f'Очков за букву: {self.get_letter_points(i)}')
+            else:
+                btn.setToolTip('')
+
+    def get_letter_points(self, let):
+        cur = self.let_con.cursor()
+        point = cur.execute(f'''SELECT value FROM letters WHERE char='{let}' ''')
+        for j in point:
+            return j[0]
 
     def update_grid(self, btns):
         for i, j in zip(self.grid, btns):
             for let, btn in zip(i, j):
+                if let != '':
+                    btn.stat = True
+                    btn.setEnabled(False)
                 btn.setText(let)
+
+    def commit_grid(self, btns, chips):
+        for i, line in enumerate(btns):
+            for j, btn in enumerate(line):
+                self.grid[i][j] = btn.text()
+        for i, chip in enumerate(chips):
+            self.curr_chips[i] = chip.text()
+
+    def raise_chips(self, btns, cursor):
+        for i in btns:
+            if i.text() != '':
+                self.chips.append(i.text())
+                print(i.text())
+        if cursor != '':
+            self.chips.append(cursor)
+            print(cursor)
+        self.chips = random.sample(self.chips, len(self.chips))
+
+    def input_word(self, btns, info):
+        res = ''
+        for i in range(info[2]):
+            if info[3]:
+                let = btns[info[0] + i][info[1]].text()
+            else:
+                let = btns[info[0]][info[1] + i].text()
+            if let == '':
+                self.log('Empty Space')
+                return False
+            res += let
+        if res in self.words:
+            self.log('Word already been!!!')
+            return False
+        self.words.append(res)
+        return self.check_word(res)
+
+    def word_points(self, info):
+        res = 0
+        cur = self.let_con.cursor()
+        for i in range(info[2]):
+            if info[3]:
+                let = self.grid[info[0] + i][info[1]]
+            else:
+                let = self.grid[info[0]][info[1] + i]
+            point = cur.execute(f'''SELECT value FROM letters WHERE char='{let}' ''')
+            for j in point:
+                res += j[0]
+        return res
 
     def close(self):
         self.let_con.close()
 
-    # TODO Change word_rus.txt to pymorphy2
     def check_word(self, word):
-        # res = self.morph.parse(word)
-        return word in self.words
+        res = self.morph.parse(word)
+        for i in res:
+            # print(i)
+            if {'NOUN', 'nomn'} in i.tag:
+                # if i.score >= 0.5:
+                print(f'PyMorphy foundt it: {i}')
+                return True
+                # else:
+                # print(f'Low score: {i}')
+            else:
+                print(f'Incorrect tags: {i}')
+        return False
+        # return word in self.words
--- wordmaker.py
+++ wordmaker.py
@@ -1,7 +1,7 @@
 import sys
 
-from PyQt5.QtGui import QColor, QFont
-from PyQt5.QtWidgets import QPushButton, QApplication
+from PyQt5.QtGui import QColor, QFont, QPalette, QPainter
+from PyQt5.QtWidgets import QPushButton, QApplication, QListWidgetItem, QDialog, QMessageBox
 
 from Board import Board
 from ui_wordmaker import Ui_MainWindow
@@ -9,15 +9,15 @@ from game import Game
 
 
 class WordMaker(Game, Ui_MainWindow):
-    def __init__(self, parent, players=[('Me', QColor(0, 255, 0), 0)]):
-        super().__init__(parent, 1000)
+    def __init__(self, parent, players=[['1st', QColor(0, 255, 0), 0]]):
+        super().__init__(parent, 50)
         print('WordMaker!')
         self.grid = []
         self.players = players
         self.c_player = 0
         self.curr_oper = None
         self.curr_letter = ''
-        # self.add_player('Saske', 255, 0, 0)
+        self.add_player('2nd', 255, 0, 0)
         for i in range(16):
             line = []
             for j in range(16):
@@ -32,66 +32,112 @@ class WordMaker(Game, Ui_MainWindow):
                 b.stat = False
                 line.append(b)
             self.grid.append(line)
-        self.board = Board()
-        self.board.generate()
 
         self.buttons = [self.b0, self.b1, self.b2, self.b3, self.b4, self.b5, self.b6]
         for i in self.buttons:
             i.clicked.connect(self.chipInput)
             i.stat = False
+
+        # Board
+        self.board = Board(self.log)
+        self.board.generate()
         self.board.next_chips()
         self.board.update_chips(self.buttons)
         self.lockGrid()
-        self.lockChips()
+        # self.lockChips()
 
         # Events
         self.wordStart.clicked.connect(self.start_word)
         self.wordEnd.clicked.connect(self.end_word)
         self.turnEnd.clicked.connect(self.end_turn)
-        self.turnCancel.clicked.connect(self.cancel_turn)
+        self.turnCancel.clicked.connect(self.cancel_word)
+
+    def log(self, msg):
+        item = QListWidgetItem(msg)
+        self.listWidget.addItem(item)
 
     def add_player(self, name, r=0, g=0, b=0):
-        self.players.append((name, QColor(r, g, b)))
+        self.players.append([name, QColor(r, g, b), 0])
         print(self.players)
 
     def next_player(self):
-        self.c_player = (self.c_player + 1) % len(self.players)
+        self.c_player = self.c_player + 1
+        if self.c_player >= len(self.players):
+            self.c_player = 0
+            if len(self.board.chips) < len(self.players) * 7:
+                self.game_over()
+
+    def game_over(self):
+        self.lockGrid()
+        self.lockChips()
+        self.wordStart.setEnabled(False)
+        self.wordEnd.setEnabled(False)
+        self.turnCancel.setEnabled(False)
+        self.turnEnd.setEnabled(False)
+        QMessageBox.about(self, 'Game over', 'В наборе закончились фишки.\nИгра завершена.')
 
     def getCurrPlayer(self):
         return self.players[self.c_player]
 
     def lockGrid(self):
-        self.setGridUnlocked(False)
-
-    def unlockGrid(self):
-        self.setGridUnlocked(True)
+        for i in self.grid:
+            for j in i:
+                j.setEnabled(False)
 
     def start_word(self):
+        b = self.comboBox.currentText() == 'Горизонтально'
+        if (b and self.wordX.value() + self.wordLen.value() - 1 >= 16) or \
+                (not b and self.wordY.value() + self.wordLen.value() - 1 >= 16):
+            self.log('Out of bounds')
+            return
         self.lockGrid()
         self.wordStart.setEnabled(False)
         self.wordEnd.setEnabled(True)
         self.turnCancel.setEnabled(True)
+        self.turnEnd.setEnabled(False)
         self.unlockChips()
-        b = self.comboBox.currentText() == 'Горизонтально'
-        if (b and self.wordX.value() + self.wordLen.value() - 1 >= 16) or \
-                (not b and self.wordY.value() + self.wordLen.value() - 1 >= 16):
-            print('Out of bounds')
-            return
-        self.curr_oper = (self.wordX.value(), self.wordY.value(), self.wordLen.value())
+        self.curr_oper = (self.wordX.value(), self.wordY.value(), self.wordLen.value(), b)
         for i in range(self.wordLen.value()):
             if b:
-                self.grid[self.wordX.value() + i][self.wordY.value()].setEnabled(True)
+                self.unlockCell(self.wordX.value() + i, self.wordY.value())
             else:
-                self.grid[self.wordX.value()][self.wordY.value() + i].setEnabled(True)
+                self.unlockCell(self.wordX.value(), self.wordY.value() + i)
 
     def end_word(self):
-        pass
+        if self.board.input_word(self.grid, self.curr_oper):
+            self.board.commit_grid(self.grid, self.buttons)
+            self.board.update_grid(self.grid)
+            self.getCurrPlayer()[2] += self.board.word_points(self.curr_oper)
+            self.log(f'У {self.getCurrPlayer()[0]} теперь {self.getCurrPlayer()[2]} очков!')
+            self.lockGrid()
+            self.wordStart.setEnabled(True)
+            self.wordEnd.setEnabled(False)
+            self.turnCancel.setEnabled(False)
+            self.turnEnd.setEnabled(True)
+        else:
+            self.log('Invalid word!')
 
     def end_turn(self):
-        pass
+        old = self.getCurrPlayer()[0]
+        # Skip
+        self.next_player()
+        self.board.raise_chips(self.buttons, self.curr_letter)
+        self.board.next_chips()
+        self.board.update_chips(self.buttons)
+        self.lockGrid()
+        self.curr_letter = ''
+        self.cursorLet.setText('')
+        # self.lockChips()
+        self.log(f'Ход закончен для "{old}".')
+        self.log(f'Следующий ход для "{self.getCurrPlayer()[0]}".')
 
-    def cancel_turn(self):
-        self.unlockChips()
+    def unlockCell(self, x, y):
+        cell = self.grid[x][y]
+        if not cell.stat:
+            cell.setEnabled(True)
+
+    def cancel_word(self):
+        # self.unlockChips()
         self.lockGrid()
         self.board.update_grid(self.grid)
         self.board.update_chips(self.buttons)
@@ -100,17 +146,21 @@ class WordMaker(Game, Ui_MainWindow):
         self.wordStart.setEnabled(True)
         self.wordEnd.setEnabled(False)
         self.turnCancel.setEnabled(False)
+        self.turnEnd.setEnabled(True)
 
     def chipInput(self):
         btn = self.sender()
         a = btn.text()
         btn.setText(self.curr_letter)
         self.curr_letter = a
-
-    def setGridUnlocked(self, locked):
-        for i in self.grid:
-            for j in i:
-                j.setEnabled(locked)
+        if self.curr_letter != '':
+            self.cursorLet.setText(f'Буква в руке: {self.curr_letter}')
+        else:
+            self.cursorLet.setText('')
+        if btn.text() != '':
+            btn.setToolTip(f'Очков за букву: {self.board.get_letter_points(btn.text())}')
+        else:
+            btn.setToolTip('')
 
     def lockChips(self):
         self.setChipsUnlocked(False)
@@ -125,7 +175,8 @@ class WordMaker(Game, Ui_MainWindow):
                 i.setEnabled(locked)
 
     def OnUpdate(self, delta):
-        print(delta)
+        pass
+        # print(delta)
         # self.board.give_chip(self.buttons)
 
     def closeEvent(self, e):
@@ -134,6 +185,15 @@ class WordMaker(Game, Ui_MainWindow):
 
     def getName(self):
         return 'WordMaker'
+
+    def paintEvent(self, e):
+        qp = QPainter(self)
+        qp.setBrush(self.getCurrPlayer()[1])
+        b = self.comboBox.currentText() == 'Горизонтально'
+        if b:
+            qp.drawRect(self.wordX.value() * 36 - 1, self.wordY.value() * 36 - 1, 36 * self.wordLen.value(), 36)
+        else:
+            qp.drawRect(self.wordX.value() * 36 - 1, self.wordY.value() * 36 - 1, 36, 36 * self.wordLen.value())
 
 
 if __name__ == '__main__':
